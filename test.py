c24dbb89603e24f5d56e5f1c2e8f1800a8a82773
Marasy-D <dizhen318@yahoo.com>
2020-11-19 11:22:42 +0800

    remove some code

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import sys
 from os import rename
 import requests
 
+
 # name = input("Your name? ")
 # print("Hello, ", name)
 print(sys.version)
@@ -19,3 +20,4 @@ print(greet("World"))
 print(greet("Corey"))
 # r = requests.get("https://coreyms.com")
 # print(r.status_code)
+print("Hello, my name is marasy.")
